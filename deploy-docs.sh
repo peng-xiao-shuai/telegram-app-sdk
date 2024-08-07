1be135191b3fdd161eb43e5a8dff6a89d4ad9971
peng-xiao-shuai <1612565136@qq.com>
2024-08-07 21:04:04 +0800

    chore: 添加 sh 文件

---
+++ deploy-docs.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# 设置变量
+REPO_URL="https://github.com/peng-xiao-shuai/telegram-sdk-docs.git"
+BRANCH="gh-pages" # 可以是你想要推送到的任何分支
+DOCS_FOLDER="docs"
+
+# 检查 DOCS_FOLDER 文件夹是否存在
+if [ ! -d "$DOCS_FOLDER" ]; then
+  echo "Docs folder does not exist!"
+  exit 1
+fi
+
+# 初始化一个新的临时目录
+TEMP_DIR=$(mktemp -d)
+echo "Using temporary directory $TEMP_DIR"
+
+# 复制docs文件夹内容到临时目录
+cp -r $DOCS_FOLDER/* $TEMP_DIR
+
+# 进入临时目录并初始化Git仓库
+cd $TEMP_DIR
+git init
+git remote add origin $REPO_URL
+
+# 添加所有文件并提交
+git add .
+git commit -m "Deploy docs"
+
+# 强制推送到远程仓库
+git push --force origin master:$BRANCH
+
+# 清理临时目录
+rm -rf $TEMP_DIR
+
+echo "Docs deployed successfully!"
