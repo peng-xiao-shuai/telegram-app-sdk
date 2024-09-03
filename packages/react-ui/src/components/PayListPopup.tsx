c89ad68b7b5f7a9ca6cfbe5c10856e31c98b5cb2
peng-xiao-shuai <1612565136@qq.com>
2024-09-03 23:20:26 +0800

    feat: 添加自定义订单支付弹窗以及订单档位支付弹窗

---
+++ packages/react-ui/src/components/PayListPopup.tsx
@@ -0,0 +1,85 @@
+import { Dialog, DialogContent, DialogFooter } from '@/components/ui/dialog';
+import { ComponentProps, type FC, memo, useCallback, useState } from 'react';
+import { Button } from './ui/button';
+import { PayListResponse } from '@telegram-sdk/ts-core';
+import { DialogPayContent } from './PayTypesPopup';
+
+const PayListItem: FC<{
+  item: PayListResponse;
+  onItemClick: (item: PayListResponse) => void;
+}> = memo(({ item, onItemClick }) => (
+  <Button
+    className="tg_sdk_ui_w-full tg_sdk_ui_mb-4 last-of-type:tg_sdk_ui_mb-0"
+    onClick={() => onItemClick(item)}
+  >
+    <b>{item.title}</b>
+    <span>{item.description}</span>
+  </Button>
+));
+
+export const PayListPopup: FC<{
+  list: PayListResponse[];
+  callback: (payItem: PayListResponse['support_token'][number]) => void;
+  onClose: () => void;
+}> = ({ callback, onClose, list }) => {
+  const [open, setOpen] = useState(true);
+  const [payContentData, setPayContentData] = useState<ComponentProps<
+    typeof DialogPayContent
+  > | null>(null);
+
+  const handleClose = () => {
+    setOpen(false);
+    setTimeout(() => {
+      onClose();
+    }, 300);
+  };
+
+  const handleItemClick = useCallback(
+    (item: PayListResponse) => {
+      setPayContentData({
+        title: item.title,
+        desc: item.description,
+        list: item.support_token,
+        callback: callback,
+      });
+    },
+    [callback]
+  );
+
+  return (
+    <Dialog open={open} onOpenChange={handleClose}>
+      <DialogContent>
+        <div
+          className={payContentData ? 'tg_sdk_ui_hidden' : 'tg_sdk_ui_block'}
+        >
+          {list.map((item) => (
+            <PayListItem
+              key={item.id}
+              item={item}
+              onItemClick={handleItemClick}
+            />
+          ))}
+        </div>
+
+        {payContentData && (
+          <DialogPayContent {...payContentData}></DialogPayContent>
+        )}
+
+        <DialogFooter>
+          <Button
+            variant="outline"
+            onClick={() => {
+              if (payContentData) {
+                setPayContentData(null);
+              } else {
+                handleClose();
+              }
+            }}
+          >
+            {payContentData ? 'Back' : 'Close'}
+          </Button>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  );
+};
---
+++ packages/react-ui/src/components/PayTypesPopup.tsx
@@ -0,0 +1,71 @@
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+} from '@/components/ui/dialog';
+import { type FC, useState } from 'react';
+import { Button } from './ui/button';
+import { PayListResponse } from '@telegram-sdk/ts-core';
+
+export const DialogPayContent: FC<{
+  title: string;
+  desc: string;
+  list: PayListResponse['support_token'];
+  callback: (payItem: PayListResponse['support_token'][number]) => void;
+}> = ({ title, desc, list, callback }) => {
+  return (
+    <>
+      <DialogHeader>
+        <DialogTitle>{title}</DialogTitle>
+        <DialogDescription>{desc}</DialogDescription>
+      </DialogHeader>
+
+      <div>
+        {list.map((item) => (
+          <Button
+            className="tg_sdk_ui_w-full tg_sdk_ui_mb-4 last-of-type:tg_sdk_ui_mb-0"
+            key={item.token}
+            onClick={() => {
+              callback(item);
+            }}
+          >
+            {item.token}
+          </Button>
+        ))}
+      </div>
+    </>
+  );
+};
+
+export const PayTypePopup: FC<{
+  title: string;
+  desc: string;
+  list: PayListResponse['support_token'];
+  callback: (payItem: PayListResponse['support_token'][number]) => void;
+  onClose: () => void;
+}> = (props) => {
+  const [open, setOpen] = useState(true);
+  const handleClose = () => {
+    setOpen(false);
+    setTimeout(() => {
+      props.onClose();
+    }, 300);
+  };
+
+  return (
+    <Dialog open={open} onOpenChange={handleClose}>
+      <DialogContent>
+        <DialogPayContent {...props}></DialogPayContent>
+
+        <DialogFooter>
+          <Button variant="outline" onClick={handleClose}>
+            Close
+          </Button>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  );
+};
