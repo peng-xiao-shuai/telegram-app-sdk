e98a5305257cf48598d7f9aec9a68dc19349b2b2
peng-xiao-shuai <1612565136@qq.com>
2024-09-03 23:22:00 +0800

    pref: 更新示例代码

--- packages/react-ui/src/App.tsx
+++ packages/react-ui/src/App.tsx
@@ -1,35 +1,98 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from 'react';
+import './App.css';
+import { login, openPopupPay } from './lib/telegram-utils';
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  useEffect(() => {
+    window._setTelegramSDKConfig({
+      debug: true,
+      appid: '5EDhUSpJU9aV9NVZRx9UXg',
+      tonConfig: {
+        manifestUrl: `https://docbphqre6f8b.cloudfront.net/tonconnect-manifest.json`,
+      },
+    });
+  }, []);
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Page></Page>
     </>
-  )
+  );
 }
 
-export default App
+const Page = () => {
+  const [initData, setInitData] = useState('');
+  const click = () => {
+    openPopupPay(
+      {
+        title: '首冲礼包',
+        message: '首冲礼包 ￥6 获得 xxx 钻石',
+        /**
+         * Stars 必须为整数
+         */
+        amount: '1',
+        /**
+         * 有概率支付失败，随机数可能会重复
+         */
+        order_id: String(Math.round(Math.random() * 10000)),
+        start: () => {
+          console.log('开始支付');
+        },
+        result: (status) => {
+          console.log('支付状态 status', status);
+        },
+      },
+      [
+        {
+          token: 'Ton',
+          amount: '0.01',
+        },
+        {
+          token: 'Stars',
+          amount: '1',
+        },
+      ]
+    );
+  };
+
+  const loginClick = () => {
+    setInitData('');
+
+    login({
+      cb: ({ status, data }) => {
+        if (status === 'success') {
+          setInitData(JSON.stringify(data));
+        }
+      },
+    });
+  };
+  return (
+    <div className="tg_sdk_ui_max-w-[80vw] tg_sdk_ui_mx-auto tg_sdk_ui_pt-10">
+      <div className="tg_sdk_ui_flex tg_sdk_ui_justify-center tg_sdk_ui_gap-4">
+        <div>
+          <button
+            className="tg_sdk_ui_px-6 tg_sdk_ui_btn tg_sdk_ui_py-2 tg_sdk_ui_border tg_sdk_ui_rounded-[0.5rem] tg_sdk_ui_border-white tg_sdk_ui_w-full tg_sdk_ui_text-white tg_sdk_ui_text-2xl"
+            onClick={click}
+          >
+            Pay
+          </button>
+        </div>
+
+        <div>
+          <button
+            className="tg_sdk_ui_px-6 tg_sdk_ui_btn tg_sdk_ui_py-2 tg_sdk_ui_border tg_sdk_ui_rounded-[0.5rem] tg_sdk_ui_border-white tg_sdk_ui_w-full tg_sdk_ui_text-white tg_sdk_ui_text-2xl"
+            onClick={loginClick}
+          >
+            Login
+          </button>
+        </div>
+      </div>
+
+      <div className="tg_sdk_ui_w-[80vw] tg_sdk_ui_mx-auto tg_sdk_ui_break-words tg_sdk_ui_text-white">
+        <div className="tg_sdk_ui_font-bold">登录返回：</div>
+        {initData}
+      </div>
+    </div>
+  );
+};
+
+export default App;
