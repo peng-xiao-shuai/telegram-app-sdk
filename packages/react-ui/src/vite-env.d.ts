d512a84c4aeab83176980c99359ddd9aa4dead48
peng-xiao-shuai <1612565136@qq.com>
2024-09-04 19:42:33 +0800

    chore: 配置项更改

--- packages/react-ui/src/vite-env.d.ts
+++ packages/react-ui/src/vite-env.d.ts
@@ -3,10 +3,12 @@ import type {
   TG_SDKOptions,
   TG_SDK_CORE,
 } from '@telegram-sdk/ts-core/dist/src/index';
+import { TG_SDK_UI } from './lib/telegram-utils';
 
 declare global {
   interface Window {
     TG_SDK_CORE: TG_SDK_CORE;
+    TG_SDK_UI: InstanceType<typeof TG_SDK_UI>;
     _setTelegramSDKConfig: (config: TG_SDKOptions) => void;
   }
 }
