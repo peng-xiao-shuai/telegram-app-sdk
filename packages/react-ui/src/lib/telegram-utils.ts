cfcd0b71cf8a7f99fb7c60842b7b74eb9c62304e
peng-xiao-shuai <1612565136@qq.com>
2024-09-04 19:41:47 +0800

    refactor: 回调函数改成 class

--- packages/react-ui/src/lib/telegram-utils.ts
+++ packages/react-ui/src/lib/telegram-utils.ts
@@ -7,60 +7,112 @@ import { fetchRequest } from './sdk-api';
 import { PayTypePopup } from '@/components/PayTypesPopup';
 import { PayListPopup } from '@/components/PayListPopup';
 import { reactRenderer } from './react-renderer';
+import { TG_SDK_CORE } from '@telegram-sdk/ts-core';
+import { version } from '../../package.json';
 
 /**
- * @description popup 回调函数，如果是 h5 则为点击事件函数。
- * @param {PayListResponse['support_token'][number]} payItem
+ * @remarks 登录成功回调载荷
  */
-const PopupPayCallback = async (
-  payItem: PayListResponse['support_token'][number]
-) => {
-  const payOptions = window.TG_SDK_CORE.PopupPayOptions;
-  window.TG_SDK_CORE.WebApp.HapticFeedback.impactOccurred('light');
-  const response = await fetchRequest<CreateOrderResponse>(
-    '/saasapi/jssdk/pay/v1/custom_order',
-    {
-      title: payOptions!.title,
-      description: payOptions!.message,
-      order_id: payOptions!.order_id,
-      amount: payOptions!.amount,
-      extra: payOptions!.extra || '',
-      token: payItem.token,
-    }
-  );
-
-  payOptions?.start?.(payItem);
-  switch (payItem.token) {
-    case 'Ton':
-      /**
-       * 是否链接，已连接直接支付
-       */
-      if (window.TG_SDK_CORE.tonConnectUI.connected) {
-        window.TG_SDK_CORE.tonTransaction(response!);
-      } else {
-        await window.TG_SDK_CORE.connect();
-        window.TG_SDK_CORE.tonTransaction(response!);
-      }
-      break;
-    case 'Stars':
-      if (!window.TG_SDK_CORE.isTG) {
-        console.warn('Not in the Telegram environment');
-
-        payOptions?.result?.({
-          status: 'paid',
-          extra: payOptions.extra,
-        });
-        return;
-      }
-      window.TG_SDK_CORE.starsTransaction(response!);
-      break;
-    case 'Usdt':
-    // TODO USDT
-  }
-};
+interface LoginSuccessPayload {
+  status: 'success';
+  data: {
+    /**
+     * @remarks 过期时间（时间戳）
+     */
+    expired_at: number;
+    token: string;
+    user_id: string;
+  };
+}
+/**
+ * @remarks 登录失败回调载荷
+ */
+interface LoginFailPayload {
+  status: 'fail';
+  /**
+   * @remarks catch 捕获到的错误信息
+   */
+  data: any;
+}
 
 /**
- * @description 唤起支付弹窗 (兼容浏览器以及TG)。非 TG 使用 Stars 支付会直接返回成功
+ * @remarks 登录成功或者失败回调函数
+ */
+interface LoginPayload {
+  cb: (payload: LoginSuccessPayload | LoginFailPayload) => void;
+}
+interface SharePayload {
+  /**
+   * @remarks 分享出去后的文字内容 默认为空
+   */
+  text?: string;
+  /**
+   * @remarks 回调函数
+   */
+  cb?: (status: 'success' | 'fail') => void;
+}
+class TG_SDK_UI extends TG_SDK_CORE {
+  constructor(options: TG_SDK_NAMESPACE.Options) {
+    super(options);
+    this.version = version;
+  }
+
+  /**
+   * @remarks popup 回调函数，如果是 h5 则为点击事件函数。
+   * @param {PayListResponse['support_token'][number]} payItem
+   */
+  private PopupPayCallback = async (
+    payItem: PayListResponse['support_token'][number]
+  ) => {
+    try {
+      this.WebApp.HapticFeedback.impactOccurred('light');
+      const response = await fetchRequest<CreateOrderResponse>(
+        '/saasapi/jssdk/pay/v1/custom_order',
+        {
+          title: this.PopupPayOptions!.title,
+          description: this.PopupPayOptions!.message,
+          order_id: this.PopupPayOptions!.order_id,
+          amount: this.PopupPayOptions!.amount,
+          extra: this.PopupPayOptions!.extra || '',
+          token: payItem.token,
+        }
+      );
+
+      this.PopupPayOptions?.start?.(payItem);
+      switch (payItem.token) {
+        case 'Ton':
+          /**
+           * 是否链接，已连接直接支付
+           */
+          if (this.tonConnectUI.connected) {
+            this.tonTransaction(response!);
+          } else {
+            await this.connect();
+            this.tonTransaction(response!);
+          }
+          break;
+        case 'Stars':
+          if (!this.isTG) {
+            console.warn('Not in the Telegram environment');
+
+            this.PopupPayOptions?.result?.({
+              status: 'paid',
+              extra: this.PopupPayOptions.extra,
+            });
+            return;
+          }
+          this.starsTransaction(response!);
+          break;
+        case 'Usdt':
+        // TODO USDT
+      }
+    } catch (error) {
+      throw this.onError('UI PopupPayCallback', error);
+    }
+  };
+
+  /**
+ * @remarks 唤起支付弹窗 (兼容浏览器以及TG)。非 TG 使用 Stars 支付会直接返回成功
  * @param {TG_SDK_NAMESPACE.OpenPayPopupPayload} options
  * @param {PayListResponse['support_token']} payTypes 支付类型
  * @example
@@ -86,164 +138,164 @@ const PopupPayCallback = async (
     },
   ])
  */
-export const openPopupPay = (
-  options: TG_SDK_NAMESPACE.OpenPayPopupPayload,
-  payTypes: PayListResponse['support_token']
-) => {
-  window.TG_SDK_CORE.PopupPayOptions = options;
+  openPopupPay = (
+    options: TG_SDK_NAMESPACE.OpenPayPopupPayload,
+    payTypes: PayListResponse['support_token']
+  ) => {
+    try {
+      this.PopupPayOptions = options;
 
-  const StarsIndex = payTypes.findIndex((item) => item.token === 'Stars');
-  /**
-   * 校验是否支持 start
-   */
-  if (
-    Number(window.TG_SDK_CORE.WebApp.version) <= 7.4 &&
-    !window.TG_SDK_CORE.debug &&
-    StarsIndex >= 0
-  ) {
-    payTypes.splice(StarsIndex, 1);
-    console.warn(
-      'Please upgrade your Telegram, Stars payment option has been removed'
-    );
-  }
-
-  reactRenderer.render(PayTypePopup, {
-    title: options.title,
-    desc: options.message,
-    list: payTypes,
-    callback: PopupPayCallback,
-  });
-};
-
-/**
- * @description 充值档位支付. 唤起充值档位弹窗
- * @example
- * window.TG_SDK.openPayList()
- */
-export const openPayList = async () => {
-  const response = await fetchRequest<{
-    list: PayListResponse[];
-  }>(
-    '/saasapi/jssdk/pay/v1/paylist',
-    {},
-    {
-      method: 'GET',
-    }
-  );
-
-  reactRenderer.render(PayListPopup, {
-    list: response.list,
-    callback: PopupPayCallback,
-  });
-};
-
-/**
- * @description 登录成功回调载荷
- */
-export interface LoginSuccessPayload {
-  status: 'success';
-  data: {
-    /**
-     * @description 过期时间（时间戳）
-     */
-    expired_at: number;
-    token: string;
-    user_id: string;
-  };
-}
-/**
- * @description 登录失败回调载荷
- */
-export interface LoginFailPayload {
-  status: 'fail';
-  /**
-   * @description catch 捕获到的错误信息
-   */
-  data: any;
-}
-
-/**
- * @description 登录成功或者失败回调函数
- */
-export interface LoginPayload {
-  cb: (payload: LoginSuccessPayload | LoginFailPayload) => void;
-}
-/**
- * @description 登录
- * @param {TG_SDK_NAMESPACE.LoginPayload} cb 登录回调函数
- * @example
- * window.TG_SDK.login({ cb: ({ status, data }) => {
- *  console.log(status, data)
-   }})
- */
-export const login = async ({ cb }: LoginPayload) => {
-  try {
-    const user_data = window.TG_SDK_CORE.debug
-      ? window.TG_SDK_CORE.WebApp.initData ||
-        'testuser#' + (window.TG_SDK_CORE.params.user_id || 9527)
-      : window.TG_SDK_CORE.WebApp.initData;
-
-    const response: LoginSuccessPayload['data'] = await fetchRequest(
-      '/saasapi/jssdk/user/v1/login',
-      {
-        app_id: window.TG_SDK_CORE.APPID,
-        invite_code: window.TG_SDK_CORE.StartData,
-      },
-      {
-        headers: {
-          Authorization: `tma ${user_data}`,
-        },
+      const StarsIndex = payTypes.findIndex((item) => item.token === 'Stars');
+      /**
+       * 校验是否支持 start
+       */
+      if (
+        Number(this.WebApp.version) <= 7.4 &&
+        !this.debug &&
+        StarsIndex >= 0
+      ) {
+        payTypes.splice(StarsIndex, 1);
+        console.warn(
+          'Please upgrade your Telegram, Stars payment option has been removed'
+        );
       }
-    );
 
-    localStorage.setItem(window.TG_SDK_CORE.params.tokenKey!, response.token);
+      reactRenderer.render(PayTypePopup, {
+        title: options.title,
+        desc: options.message,
+        list: payTypes,
+        callback: this.PopupPayCallback,
+      });
+    } catch (error) {
+      throw this.onError('UI openPopupPay', error);
+    }
+  };
 
-    cb({
-      status: 'success',
-      data: response,
-    });
-  } catch (error) {
-    cb({
-      status: 'fail',
-      data: error,
-    });
-    throw window.TG_SDK_CORE.onError('UI login', error);
+  /**
+   * @remarks 充值档位支付. 唤起充值档位弹窗
+   * @example
+   * window.TG_SDK.openPayList()
+   */
+  async openPayList() {
+    try {
+      const response = await fetchRequest<{
+        list: PayListResponse[];
+      }>(
+        '/saasapi/jssdk/pay/v1/paylist',
+        {},
+        {
+          method: 'GET',
+        }
+      );
+
+      reactRenderer.render(PayListPopup, {
+        list: response.list,
+        callback: this.PopupPayCallback,
+      });
+    } catch (error) {
+      throw this.onError('UI openPayList', error);
+    }
   }
-};
 
-export interface SharePayload {
   /**
-   * @description 分享出去后的文字内容 默认为空
+   * @remarks 登录
+   * @param {TG_SDK_NAMESPACE.LoginPayload} cb 登录回调函数
+   * @example
+   * window.TG_SDK.login({
+   *  cb: ({ status, data }) => {
+   *    console.log(status, data)
+   *  }
+   * })
    */
-  text?: string;
+  login({ cb }: LoginPayload): Promise<Parameters<LoginPayload['cb']>[0]> {
+    return new Promise((resolve) => {
+      (async () => {
+        try {
+          const user_data = this.debug
+            ? this.WebApp.initData ||
+              'testuser#' + (this.params.user_id || 9527)
+            : this.WebApp.initData;
+
+          const response: LoginSuccessPayload['data'] = await fetchRequest(
+            '/saasapi/jssdk/user/v1/login',
+            {
+              app_id: this.APPID,
+              invite_code: this.StartData,
+            },
+            {
+              headers: {
+                Authorization: `tma ${user_data}`,
+              },
+            }
+          );
+
+          localStorage.setItem(this.params.tokenKey!, response.token);
+
+          cb({
+            status: 'success',
+            data: response,
+          });
+          resolve({
+            status: 'success',
+            data: response,
+          });
+        } catch (error) {
+          cb({
+            status: 'fail',
+            data: error,
+          });
+          resolve({
+            status: 'fail',
+            data: error,
+          });
+          throw this.onError('UI login', error);
+        }
+      })();
+    });
+  }
+
   /**
-   * @description 回调函数
+   * @remarks 分享
+   * @param {Parameters<TG_SDK_NAMESPACE.SharePayload>[0]} payload
+   * @example
+   * window.TG_SDK.share()
+   * window.TG_SDK.share({ text: '描述内容，这段内容会在输入框内', cb: (status) => {
+   *  console.log(status)
+   * }})
    */
-  cb?: (status: 'success' | 'fail') => void;
+  share({
+    text,
+    cb,
+  }: SharePayload): Promise<Parameters<NonNullable<SharePayload['cb']>>[0]> {
+    return new Promise((resolve) => {
+      (async () => {
+        try {
+          const { link }: { link: string } = await fetchRequest(
+            '/saasapi/jssdk/user/v1/share'
+          );
+
+          this.WebApp.openTelegramLink(
+            encodeURI(
+              `https://t.me/share/url?url=${link}${text ? '&text=' + text : ''}`
+            )
+          );
+          cb?.('success');
+          resolve('success');
+        } catch (error) {
+          cb?.('fail');
+          resolve('fail');
+          throw this.onError('share', error);
+        }
+      })();
+    });
+  }
 }
-/**
- * @description 分享
- * @param {Parameters<TG_SDK_NAMESPACE.SharePayload>[0]} payload
- * @example
- * window.TG_SDK.share()
- * window.TG_SDK.share({ text: '描述内容，这段内容会在输入框内', cb: (status) => {
- *  console.log(status)
- * }})
- */
-export const share = async ({ text, cb }: SharePayload) => {
-  try {
-    const { link }: { link: string } = await fetchRequest(
-      '/saasapi/jssdk/user/v1/share'
-    );
 
-    window.TG_SDK_CORE.WebApp.openTelegramLink(
-      encodeURI(
-        `https://t.me/share/url?url=${link}${text ? '&text=' + text : ''}`
-      )
-    );
-    cb?.('success');
-  } catch (error) {
-    cb?.('fail');
-    throw window.TG_SDK_CORE.onError('share', error);
-  }
+export { TG_SDK_UI };
+
+export type {
+  LoginSuccessPayload,
+  LoginFailPayload,
+  LoginPayload,
+  SharePayload,
 };
