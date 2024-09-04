2a2dcf09865172e7a82c4dfd18ecea1b496427b2
peng-xiao-shuai <1612565136@qq.com>
2024-09-04 22:28:26 +0800

    chore: 基础构建配置

--- packages/react-ui/vite.config.ts
+++ packages/react-ui/vite.config.ts
@@ -1,10 +1,22 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import path from 'path';
-
+import dts from 'vite-plugin-dts';
+import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js';
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    dts({
+      insertTypesEntry: true,
+      outDir: 'dist', // 确保输出目录为 dist
+      entryRoot: 'src', // 确保入口根目录为 src
+      tsconfigPath: path.resolve(__dirname, 'tsconfig.node.json'), // 指定 tsconfig 文件路径
+      rollupTypes: true,
+      bundledPackages: ['@telegram-sdk/ts-core'],
+    }),
+    cssInjectedByJsPlugin(),
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
@@ -17,10 +29,13 @@ export default defineConfig({
     ),
   },
   build: {
+    minify: true,
     lib: {
       entry: path.resolve(__dirname, 'src/index.ts'),
-      name: 'ReactUI',
-      fileName: (format) => `react-ui.${format}.js`,
+      name: 'TG_SDK_UI',
+      formats: ['es', 'cjs', 'iife'],
+      fileName: (format) =>
+        format === 'iife' ? 'telegram-sdk-ui.js' : `index.${format}.js`,
     },
     rollupOptions: {
       output: {
@@ -29,6 +44,12 @@ export default defineConfig({
           'react-dom': 'ReactDOM',
         },
       },
+    },
+    cssCodeSplit: false,
+  },
+  css: {
+    modules: {
+      localsConvention: 'camelCaseOnly',
     },
   },
 });
