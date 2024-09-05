c7bfee59d91719c1e96aa03de9138ffc8f708882
peng-xiao-shuai <1612565136@qq.com>
2024-09-05 14:38:32 +0800

    chore: git 忽略更新

--- packages/ts-core/dist/src/index.d.ts
+++
@@ -1,1 +0,0 @@
-export type { TG_SDKOptions, TG_SDK_NAMESPACE, TG_SDK } from './core';
--- packages/ts-core/dist/src/utils/model.d.ts
+++
@@ -1,6 +0,0 @@
-import type { TG_SDK_NAMESPACE } from '../core';
-export declare function createButton(this: any, buttons: TG_SDK_NAMESPACE.ParamsPopupButton[]): void;
-export declare function showModal({ title, }: {
-    title: string;
-}): void;
-export declare function closeModal(): void;
